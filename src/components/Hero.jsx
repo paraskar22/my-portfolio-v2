986b5ff9c9827d97cd6603fdc9dadce2abc0f573
Vaishali Paraskar <potterheadparaskar@Vaishalis-MacBook-Pro.local>
2025-04-19 16:31:22 -0400

    Initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -7,7 +6,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -15,17 +14,18 @@ const Hero = () => {
         </div>
 
         <div>
-      <h1 className="text-white text-3xl sm:text-5xl font-semibold">
-        Hi, I'm <span className="text-[#915EFF] text-2xl sm:text-4xl font-bold">Vaishali Paraskar</span>
-      </h1>
-      <p className="mt-2 text-white-100 text-sm sm:text-base leading-relaxed">
-        I am a Data Analyst <br className="sm:block hidden" />
-        I develop AI-driven solutions, statistical modeling, and data analytics
-      </p>
-    </div>
+          <h1 className="text-white text-3xl sm:text-5xl font-semibold">
+            Hi, I'm <span className="text-[#915EFF] text-2xl sm:text-4xl font-bold">Vaishali Paraskar</span>
+          </h1>
+          <p className="mt-2 text-white-100 text-sm sm:text-base leading-relaxed">
+            I am a Data Analyst <br className="sm:block hidden" />
+            I develop AI-driven solutions, statistical modeling, and data analytics
+          </p>
+        </div>
       </div>
 
-      <div className="absolute w-full flex justify-center items-center top-[250px]">
+      {/* Moved image to the left */}
+      <div className="absolute top-[250px] left-[18%]">
         <motion.div
           animate={{
             y: [0, -20, 0],
@@ -47,7 +47,7 @@ const Hero = () => {
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[40px] h-[70px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
