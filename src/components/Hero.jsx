b870f6253dc2a2c750e3d5c63c8ed283c7dad1bd
Vaishali paraskar <167244536+paraskar22@users.noreply.github.com>
2025-04-21 13:43:40 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
 
 const Hero = () => {
   return (
@@ -66,4 +66,4 @@ const Hero = () => {
   );
 };
 
-export default Hero;
+export default SectionWrapper(Hero, "hero");
